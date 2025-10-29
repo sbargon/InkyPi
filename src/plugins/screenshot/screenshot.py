f2c88ae68350ad2cdb1cebec28b7c0edd1cf0116
Stuart Bargon <sbargon@gmail.com>
2025-10-29 11:21:29 +1100

    Fullscreen option

--- src/plugins/screenshot/screenshot.py
+++ src/plugins/screenshot/screenshot.py
@@ -15,7 +15,7 @@ class Screenshot(BasePlugin):
         if not settings.get('fullScreen') or settings.get('fullScreen') == "false":
             dimensions = device_config.get_resolution()
         else:
-            dimensions = [-1,-1]
+            dimensions = []
         if device_config.get_config("orientation") == "vertical":
             dimensions = dimensions[::-1]
 
